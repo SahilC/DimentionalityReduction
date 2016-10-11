fe8c8158f0f72e04cb93ef8108e5bce1b71cbd51
Sahil Chelaramani <sahilc.2200@gmail.com>
2016-10-11 14:13:16 +0530

    Updates to Q2

--- question2.r
+++ question2.r
@@ -1,7 +1,7 @@
 setwd('~/Code/SMAI/BayesianNets')
 
 dense_to_sparse <- function(data,nfeatures) {
-  A = matrix(0,800,nfeatures)
+  A = matrix(0,nrow(data),nfeatures)
   for (k in 1:dim(data)[1]) {
     for (i in 1:(dim(data)[2])) {
       if(!is.na(data[k,i]) && data[k,i]<= nfeatures) {
@@ -44,8 +44,9 @@ fisher_components <- function(data, labels) {
   eig <- eigen(invSw)
   vectors <- as.matrix(eig$vectors[,1:50])
   A[,ncol(A)] <- NULL
-  final_data <- t(t(vectors) %*% t(A))
-  return(final_data)
+  # final_data <- t(t(vectors) %*% t(A))
+  # return(final_data)
+  return(vectors)
 }
 
 gaussian_naive_bayes <- function(data, test) {
@@ -60,9 +61,46 @@ gaussian_naive_bayes <- function(data, test) {
   
   Var1 <- as.matrix(apply(positive, 2, function(y) var(y)))
   Var2 <- as.matrix(apply(negative, 2, function(y) var(y)))
+  
+  U <- (X - Mu1)^2
+  V <- (Y - Mu2)^2
+  
+  Var1 <- 2*Var1^2
+  Var2 <- 2*Var2^2
+  
+  U <- -1*U/Var1
+  V <- -1*V/Var2
+  
+  U <- exp(U)
+  V <- exp(V)
+  
+  U <- (1/sqrt(pi*Var1))*U
+  V <- (1/sqrt(pi*Var2))*V
+  
   mistake = 0
   total = 0
   for(i in 1:100) {
+    vec <- test[i,]
+    #predict <- test[i,length(vec)]
+    pos_score = 1
+    neg_score = 1
+    #print(vec)
+    for(j in 1:(length(vec)-1)) {
+      pos_score = pos_score + log()
+      neg_score = neg_score + log(num_total/num_neg)
+    }
+    pos_score = pos_score + log(nrow(data)/nrow(positive))
+    neg_score = neg_score + log(nrow(data)/nrow(negative))
+    
+    if(length(pos_score) == 0) {
+      naive_predict = -1
+    } else {
+      if((pos_score - neg_score)  >= 0.01) {
+        naive_predict = -1
+      } else {
+        naive_predict = 1
+      }
+    }
     if(naive_predict != predict) {
       mistake = mistake + 1
     }
@@ -74,5 +112,7 @@ gaussian_naive_bayes <- function(data, test) {
 data <- read.table("Datasets/dorothea_train.data", header = FALSE, sep = " ", col.names = paste0("V",seq_len(6100)), fill = TRUE)
 labels <- read.table("Datasets/dorothea_train.labels", header = FALSE, sep = " ") 
 #principle_components(data)
-final_data <- fisher_components(data,labels)
+vectors <- fisher_components(data,labels)
+test <- read.table("Datasets/dorothea_valid.data", header = FALSE, sep = " ", col.names = paste0("V",seq_len(6100)), fill = TRUE)
+test <- dense_to_sparse(test,200)
 
